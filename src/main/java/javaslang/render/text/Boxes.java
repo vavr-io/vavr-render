80342525c7c736103453ef81013c0aa3dde6a77e
romildo <malaquias@gmail.com>
2016-02-15 13:09:29 -0200

    Fix layout of trees

--- src/main/java/javaslang/render/text/Boxes.java
+++ src/main/java/javaslang/render/text/Boxes.java
@@ -1,7 +1,10 @@
 package javaslang.render.text;
 
+import javaslang.Tuple;
+import javaslang.Tuple2;
 import javaslang.collection.List;
 import javaslang.collection.Tree;
+import javaslang.collection.Tree.Node;
 
 import java.util.Objects;
 
@@ -11,19 +14,51 @@ public final class Boxes {
     }
 
     public static <T> Box box(Tree<T> tree) {
-        return box(tree, true);
+        Objects.requireNonNull(tree, "tree is null");
+        if (tree.isEmpty())
+            return Box.of("▣");
+        Tuple2<Box, Integer> x = nodeToBox((Node<T>) tree);
+        return Box.col(Box.row(Box.of('┌'), Box.of('─', x._2-2, 1), Box.of('┐')), x._1);
     }
 
-    private static <T> Box box(Tree<T> tree, boolean isRoot) {
-        Objects.requireNonNull(tree, "tree is null");
-        if (tree.isEmpty()) {
-            return Box.of("▣");
-        }
-        final Box b = Box.of(tree.getValue().toString()).frame(isRoot, tree.isLeaf());
+    private static <T> Tuple2<Box, Integer> nodeToBox(Node<T> tree) {
+        Box b = Box.of(tree.getValue().toString());
+        Box side = Box.of('│', 1, b.height());
+        b = Box.row(side, b, side);
+        int b_width = b.width();
         if (tree.isLeaf()) {
-            return b;
+            b = Box.col(b, Box.row(Box.of('└'), Box.of('─', b_width - 2, 1), Box.of('┘')));
+            return Tuple.of(b, b_width);
         }
-        final List<Box> lst = tree.getChildren().map(t -> box(t, false));
-        return b.connect(lst);
+        List<Tuple2<Box,Integer>> children = tree.getChildren().map(t -> nodeToBox(t));
+        if (children.length() == 1) {
+            Tuple2<Box,Integer> c = children.head();
+            Box c_box = c._1;
+            int c_width = c_box.width();
+            int c_node_width = c._2;
+            b = Box.col(b, Box.of(mkheader(b_width, '└', '─', '┬', '┘', b_width < c_width ? 0 : 1)));
+            c_box = Box.col(Box.of(mkheader(c_node_width, '┌', '─', '┴', '┐', c_width < b_width ? 0 : 1)), c_box);
+            Box z = Box.col(b, Box.of('│'), c_box);
+            return Tuple.of(z, b_width);
+        }
+        b = Box.col(b, Box.of(mkheader(b_width, '└', '─', '┬', '┘', 0)));
+        List<Box> lst = children.map(p -> {
+            Box k = p._1;
+            int m = p._2;
+            k = Box.col(Box.of(mkheader(m, '┌', '─', '┴', '┐', 0)), k);
+            return k;
+        });
+        b = b.connect(lst);
+        return Tuple.of(b, b_width);
+    }
+
+    private static String mkheader(int width, char a, char m1, char m2, char z, int control) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(a);
+        for (int i = 1; i < width - 1; i++)
+            builder.append(m1);
+        builder.append(z);
+        builder.setCharAt((width-control)/2, m2);
+        return builder.toString();
     }
 }
--- src/test/java/javaslang/render/text/BoxesTest.java
+++ src/test/java/javaslang/render/text/BoxesTest.java
@@ -15,6 +15,86 @@ public class BoxesTest {
     @Test
     public void shouldCreateBoxesOfTree() {
 
+        final Tree<String> tree1 = Tree.of("TEST");
+        assertThat(Boxes.box(tree1).toString()).isEqualTo("" +
+                "┌────┐\n" +
+                "│TEST│\n" +
+                "└────┘");
+
+        final Tree<String> tree2 = Tree.of("XXXX*XXXX", Tree.of("AA=AA"));
+        assertThat(Boxes.box(tree2).toString()).isEqualTo("" +
+                "┌─────────┐\n" +
+                "│XXXX*XXXX│\n" +
+                "└────┬────┘\n" +
+                "     │     \n" +
+                "  ┌──┴──┐  \n" +
+                "  │AA=AA│  \n" +
+                "  └─────┘  ");
+
+        final Tree<String> tree3 = Tree.of("XXXX*XXX", Tree.of("AA=AA"));
+        assertThat(Boxes.box(tree3).toString()).isEqualTo("" +
+                "┌────────┐\n" +
+                "│XXXX*XXX│\n" +
+                "└───┬────┘\n" +
+                "    │     \n" +
+                " ┌──┴──┐  \n" +
+                " │AA=AA│  \n" +
+                " └─────┘  ");
+
+        final Tree<String> tree4 = Tree.of("XXXX*XXXX", Tree.of("AA=A"));
+        assertThat(Boxes.box(tree4).toString()).isEqualTo("" +
+                "┌─────────┐\n" +
+                "│XXXX*XXXX│\n" +
+                "└────┬────┘\n" +
+                "     │     \n" +
+                "  ┌──┴─┐   \n" +
+                "  │AA=A│   \n" +
+                "  └────┘   ");
+
+        final Tree<String> tree5 = Tree.of("AA=AA", Tree.of("XXXX*XXXX"));
+        assertThat(Boxes.box(tree5).toString()).isEqualTo("" +
+                "  ┌─────┐  \n" +
+                "  │AA=AA│  \n" +
+                "  └──┬──┘  \n" +
+                "     │     \n" +
+                "┌────┴────┐\n" +
+                "│XXXX*XXXX│\n" +
+                "└─────────┘");
+
+        final Tree<String> tree6 = Tree.of("AA=A", Tree.of("XXXX*XXXX"));
+        assertThat(Boxes.box(tree6).toString()).isEqualTo("" +
+                "  ┌────┐   \n" +
+                "  │AA=A│   \n" +
+                "  └──┬─┘   \n" +
+                "     │     \n" +
+                "┌────┴────┐\n" +
+                "│XXXX*XXXX│\n" +
+                "└─────────┘");
+
+        final Tree<String> tree7 = Tree.of("AA=AA", Tree.of("XXXX*XXX"));
+        assertThat(Boxes.box(tree7).toString()).isEqualTo("" +
+                " ┌─────┐  \n" +
+                " │AA=AA│  \n" +
+                " └──┬──┘  \n" +
+                "    │     \n" +
+                "┌───┴────┐\n" +
+                "│XXXX*XXX│\n" +
+                "└────────┘");
+
+        final Tree<String> ast =
+                Tree.of("IfExp",
+                        Tree.of("BoolExp: false"),
+                        Tree.of("IntExp: 10"),
+                        Tree.of("IntExp: 20"));
+        assertThat(Boxes.box(ast).toString()).isEqualTo("" +
+                "                 ┌─────┐                  \n" +
+                "                 │IfExp│                  \n" +
+                "                 └──┬──┘                  \n" +
+                "        ┌───────────┴──┬────────────┐     \n" +
+                "┌───────┴──────┐ ┌─────┴────┐ ┌─────┴────┐\n" +
+                "│BoolExp: false│ │IntExp: 10│ │IntExp: 20│\n" +
+                "└──────────────┘ └──────────┘ └──────────┘");
+
         final Tree<String> tree =
                 Tree.of("Ann",
                         Tree.of("Mary",
@@ -34,21 +114,21 @@ public class BoxesTest {
 
         final String boxesString = Boxes.box(tree).toString();
         assertThat(boxesString).isEqualTo("" +
-                "                            ┌───┐                            \n" +
-                "                            │Ann│                            \n" +
-                "                            └─┬─┘                            \n" +
-                "             ┌────────────────┴──────┬────────────────┐      \n" +
-                "          ┌──┴─┐                  ┌──┴──┐       ┌─────┴─────┐\n" +
-                "          │Mary│                  │Peter│       │Christopher│\n" +
-                "          └──┬─┘                  └──┬──┘       └─────┬─────┘\n" +
-                "   ┌───────┬─┴───────┐          ┌────┴────┐           │      \n" +
-                "┌──┴─┐  ┌──┴──┐ ┌────┴───┐  ┌───┴───┐ ┌───┴───┐   ┌───┴──┐   \n" +
-                "│John│  │Karen│ │ Steven │  │ Paul  │ │Anthony│   │Samuel│   \n" +
-                "└──┬─┘  └──┬──┘ │ Abbot  │  │Palucci│ └───────┘   └──────┘   \n" +
-                "   │       │    │Braddock│  └───────┘                        \n" +
-                "┌──┴──┐ ┌──┴──┐ └────────┘                                   \n" +
-                "│Avila│ │Frank│                                              \n" +
-                "└─────┘ └─────┘                                              "
+                "                           ┌───┐                            \n" +
+                "                           │Ann│                            \n" +
+                "                           └─┬─┘                            \n" +
+                "             ┌───────────────┴──────┬────────────────┐      \n" +
+                "          ┌──┴─┐                 ┌──┴──┐       ┌─────┴─────┐\n" +
+                "          │Mary│                 │Peter│       │Christopher│\n" +
+                "          └──┬─┘                 └──┬──┘       └─────┬─────┘\n" +
+                "   ┌───────┬─┴───────┐         ┌────┴────┐           │      \n" +
+                "┌──┴─┐  ┌──┴──┐ ┌────┴───┐ ┌───┴───┐ ┌───┴───┐   ┌───┴──┐   \n" +
+                "│John│  │Karen│ │ Steven │ │ Paul  │ │Anthony│   │Samuel│   \n" +
+                "└──┬─┘  └──┬──┘ │ Abbot  │ │Palucci│ └───────┘   └──────┘   \n" +
+                "   │       │    │Braddock│ └───────┘                        \n" +
+                "┌──┴──┐ ┌──┴──┐ └────────┘                                  \n" +
+                "│Avila│ │Frank│                                             \n" +
+                "└─────┘ └─────┘                                             "
         );
     }
 }
